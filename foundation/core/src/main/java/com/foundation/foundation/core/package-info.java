2c638400ceeedf69a18415ee2b9f5f1270e9ad22
krishna <krishna@krishna>
2016-01-07 23:22:57 -0600

    testing

--- foundation/core/src/main/java/com/foundation/foundation/core/package-info.java
+++ foundation/core/src/main/java/com/foundation/foundation/core/package-info.java
@@ -13,6 +13,8 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
+
+//Testing commiting
 @Version("1.0")
 @Export(optional = "provide:=true")
 package com.foundation.foundation.core;
